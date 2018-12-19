db0b34260a6213dfa33db035aec5aa04d226c3d6
Jisan Ahmed <jisan.mia@northsouth.edu>
2018-12-19 11:29:48 +0000

    Modified main function.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "SelectionSort.h"
+#include "InsertionSort.h"
 using namespace std;
 
 int main()
@@ -9,6 +10,11 @@ SelectionSort jisan;
     int a[8] = {14,33,27,10,35,19,48,44};
 int n = sizeof(a)/sizeof(a[0]);
     jisan.SelectionSorter(a,n);
+
+InsertionSort jisan1;
+int b[8] = {14,33,27,10,35,19,48,44};
+int n1= sizeof(b)/sizeof(b[0]);
+    jisan1.InsertionSorter(b,n1);
 
     return 0;
 }
